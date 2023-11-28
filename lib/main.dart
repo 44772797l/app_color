e736a21be86c15a03cfe44ea1479c8b609f692df
pedroditer <pedroditersimon@gmail.com>
2023-11-28 01:59:46 -0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:app_prueba/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() => runApp(MyApp());
+void  main() async {
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 //texto ejemplo para ver si funciona git
 // cabeza de termo
